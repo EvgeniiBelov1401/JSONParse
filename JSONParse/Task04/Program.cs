7d3f453f5d7140fbe2e7d62b33a78006e18ba3ac
EvgeniiBelov <evgeniibelov@gmail.com>
2025-04-28 17:43:31 +0300

    сделал Динамический парсинг JSON из файла(без классов) Через Newtonsoft.Json

---
+++ JSONParse/Task04/Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+internal class Program
+{
+    //Динамический парсинг JSON из файла(без классов) Через Newtonsoft.Json
+    private static void Main(string[] args)
+    {
+
+        var fileName = "person.json";
+        var fileRootDirectory = Directory.GetParent(Directory.GetCurrentDirectory())!.Parent?.Parent?.FullName;
+        var filePath = Path.Combine(fileRootDirectory!, "Source", fileName);
+
+        var json = File.ReadAllText(filePath);
+
+        JObject obj = JObject.Parse(json);
+        var name = (string)obj["name"]!;
+        var age = (int)obj["age"]!;
+        var country = (string)obj["address"]!["country"]!;
+        var city = (string)obj["address"]!["city"]!;
+        JArray skills = (JArray)obj["skills"]!;
+
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Age: {age}");
+        Console.WriteLine($"Country: {country}");
+        Console.WriteLine($"City: {city}");
+        Console.WriteLine($"Skills: {string.Join(", ",skills)}");
+    }
+
+}
